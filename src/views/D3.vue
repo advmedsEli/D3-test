e367abf4a60684b4d094631b5b87e3361bfe0e64
Eli <eli@advmeds.com>
2020-10-14 18:05:01 +0800

    新增圖片

--- src/views/D3.vue
+++ src/views/D3.vue
@@ -3,3 +3,23 @@
     <h1>D3 - TPR</h1>
   </div>
 </template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  name: 'D3',
+  data () {
+    return {
+      startDate: '2020/10/01',
+      endDate: '2020/10/20',
+      datalist: [
+        { date: '2020/10/01 09:30' }
+      ]
+    }
+  },
+  mounted () {
+    console.log(d3)
+  }
+}
+</script>
